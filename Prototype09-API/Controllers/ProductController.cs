f356a60274252bb1612816d5a8aae1d9a56fb990
SomilaY <ST10091012@vcconnect.edu.za>
2023-10-01 12:18:22 +0000

    Add project files.

---
+++ Prototype09-API/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Prototype09_API.Models;
+
+namespace Prototype09_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Product")]
+        public string ProductName()
+        {
+            return "Milk";
+        }
+        [HttpGet]
+        [Route("All")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product> {
+         new Product(1,"Coke"),
+         new Product(2, "Meat"),
+         new Product(3, "Coffee"),
+         new Product(4, "Porridge"),
+         };
+        }
+    }
+}
